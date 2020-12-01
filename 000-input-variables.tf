74b31339fb435d3ad98657ab93ac428963ced304
Jeremy Chauvin <jeremychauvin10@gmail.com>
2020-12-01 21:02:54 +0100

    feat(): add description on var and add list on port

--- 000-input-variables.tf
+++ 000-input-variables.tf
@@ -1,31 +1,73 @@
-variable "servergroup_id" {
-  type = string
-}
-
 variable "key_pair_name" {
   type = string
+  description = <<EOF
+The name of the ssh key referenced in openstack
+EOF
 }
 
-variable "image" {
-  type = object({ id=string, name=string })
+variable "image_id" {
+  type = string
+  description = <<EOF
+The image's id referenced in openstack
+EOF
 }
 
-variable "instance" {
-  type = object({ name=string, flavor_name=string, port_name=string})
+variable "name" {
+  type = string
+  description = <<EOF
+Instance's name
+EOF
 }
 
-variable "security_group_ids" {
-  type = list(string)
+variable "flavor_name" {
+  type = string
+  description = <<EOF
+Instance's flavor name referenced in openstack
+EOF
 }
 
 variable "public_ip_network" {
   type = string
+  description = <<EOF
+The name of the network who give floating IPs
+EOF
 }
 
-variable "network_id" {
-  type = string
+variable "ports" {
+  type = list(any)
+  description = <<EOF
+The ports list can take object like this :
+ports:
+- name: `required`
+  network_id: `required`
+  subnet_id: `required`
+  admin_state_up: `optional`, `default: true`
+  security_group_ids: `optional`, `default: []`
+  ip_address: `optional`, `default: []`
+EOF
+  default = []
 }
 
-variable "subnet_id" {
-  type = string
+variable "block_device_volume_size" {
+  type = number
+  description = <<EOF
+The volume size of block device
+EOF
+  default = 20
+}
+
+variable "block_device_delete_on_termination" {
+  type = bool
+  description = <<EOF
+Delete block device when instance is shut down
+EOF
+  default = true
+}
+
+variable "server_groups" {
+  type = list(string)
+  description = <<EOF
+List of server group id
+EOF
+  default = []
 }
